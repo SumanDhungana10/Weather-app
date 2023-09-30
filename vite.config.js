d6119dba509072d4c8c4b068c8824ba573a349d1
Suman Dhungana <dhunganasuman226@gmail.com>
2023-09-30 10:42:03 +0545

    update homepage

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Weather-app/dist",
+  base: "https://sumandhungana10.github.io/Weather-app/",
   plugins: [react()],
 })
